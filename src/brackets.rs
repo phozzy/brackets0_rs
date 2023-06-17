ea91d1c099ad54614eb4d0cb5ed6eddee4c857f9
phozzy <arthur.fayzullin@gmail.com>
2023-06-17 14:15:42 +0200

    Add 2 basic functions for Brackets

---
+++ src/brackets.rs
@@ -0,0 +1,45 @@
+use core::num::{dec2flt::number, self};
+
+struct Brackets {
+    n_pairs: Iterator,
+    pairs_list: Option<String>,
+}
+
+impl Iterator for Brackets {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
+    }
+}
+
+impl Brackets {
+    fn new(level: usize) -> Brackets {
+        let start: usize = 2.pow(level - 1) - 1;
+        let end: usize = 2 * (1 - 4.pow(level - 1)) / (1 - 4);
+        Brackets {
+            n_pairs: start..=end,
+            pairs_list: None,
+        }
+    }
+    fn valid_pairs(mut number: usize) -> Option<String> {
+        let mut count: usize = 0;
+        let mut pairs: String = String::from("(");
+        while number != 0 {
+            if number & 1 == 1 {
+                count += 1;
+                pairs.push("(");
+            } else {
+                count -= 1;
+                pairs.push(")");
+            }
+            number >> 1;
+        }
+        if count == 0 {
+            pairs.push(")");
+            Some(pairs)
+        } else {
+            None
+        }
+    }
+}
